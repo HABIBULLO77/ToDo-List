b95025eaf505531d16600f3ee9ff4ade745a3ca7
HABIBULLO77 <h.hamidov77777@gmail.com>
2022-03-30 01:06:04 +0900

    Login/Logout

--- base/views.py
+++ base/views.py
@@ -1,12 +1,23 @@
 from curses.ascii import CR
 from pyexpat import model
-from attr import field
+from attr import field, fields
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
 from .models import Task
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
