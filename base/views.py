4595720a2f8c15165ace66b1de9209b80429832a
HABIBULLO77 <h.hamidov77777@gmail.com>
2022-04-03 20:37:54 +0900

    search

--- base/views.py
+++ base/views.py
@@ -50,7 +50,8 @@ class TaskList(LoginRequiredMixin, ListView):
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+            context['tasks'] = context['tasks'].filter(
+                title__startswith=search_input)
 
         context['search_input'] = search_input
         return context
